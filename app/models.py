f76c17fccdba83058adaff2bb2ec0afd88700d8c
unohayato <mamidori1p1p@gmail.com>
2022-10-17 18:55:42 +0900

    migrate

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  title = models.CharField('タイトル', max_length=200)
+  text = models.TextField('内容')
+  created = models.DateTimeField('作成日', default=timezone.now())
+  
+  def __str__(self):
+    return self.title
+  
